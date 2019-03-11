589fffa60f47924f9acb512832a5b11a71a8370e
Olimpio\Asafe <asafeolimpio@gmail.com>
2019-03-11 19:42:39 +0100

    Servidor Reparado

---
+++ FacegramEE/FacegramEE-ejb/build/generated-sources/ap-source-output/model/Amigo_.java
@@ -0,0 +1,16 @@
+package model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import model.Usuario;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-11T19:29:35")
+@StaticMetamodel(Amigo.class)
+public class Amigo_ { 
+
+    public static volatile SingularAttribute<Amigo, Usuario> idUsuario1;
+    public static volatile SingularAttribute<Amigo, Usuario> idUsuario2;
+    public static volatile SingularAttribute<Amigo, Integer> id;
+
+}
---
+++ FacegramEE/FacegramEE-ejb/build/generated-sources/ap-source-output/model/Post_.java
@@ -0,0 +1,19 @@
+package model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-11T19:29:35")
+@StaticMetamodel(Post.class)
+public class Post_ { 
+
+    public static volatile SingularAttribute<Post, String> texto;
+    public static volatile SingularAttribute<Post, Boolean> tipoContenido;
+    public static volatile SingularAttribute<Post, Integer> idUsuario;
+    public static volatile SingularAttribute<Post, String> titulo;
+    public static volatile SingularAttribute<Post, byte[]> imagen;
+    public static volatile SingularAttribute<Post, Boolean> tipoUsuario;
+    public static volatile SingularAttribute<Post, Integer> id;
+
+}
