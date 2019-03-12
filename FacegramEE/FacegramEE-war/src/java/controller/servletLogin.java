76d17316bbc6a0ec5aa2abe6fe821c7eed881573
sergio <chivo.sergio.jesus@gmail.com>
2019-03-12 17:59:20 +0100

    test

---
+++ FacegramEE/FacegramEE-war/src/java/controller/servletLogin.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Usuario;
+
+/**
+ *
+ * @author Jimmy-Dev
+ */
+@WebServlet("/login")
+public class servletLogin extends HttpServlet {
+
+    private static final String SUCCESS = "index.jsp";
+    private static final String ERROR = "login.jsp";
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        try {
+            //Datos del formulario
+            Usuario usuario = new Usuario();
+            usuario.setCorreo(request.getParameter("correo"));
+            usuario.setPass(request.getParameter("pass"));
+
+            //Buscar
+            usuario = svc.buscar(usuario);
+
+            if (usuario == null) {
+                request.setAttribute("error", "Usuario/contraseña incorrecta");
+                request.getRequestDispatcher(ERROR).forward(request, response);
+            } else {
+                //Guardar en la sesión
+                request.getSession().setAttribute(LoginFilter.ATT_USER, usuario);
+                request.getRequestDispatcher(SUCCESS).forward(request, response);
+            }
+
+        } catch (Exception ex) {
+            request.setAttribute("error", "Error al conectar");
+            request.getRequestDispatcher(ERROR).forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- FacegramEE/FacegramEE-war/web/jsp/amigos.jsp
+++ FacegramEE/FacegramEE-war/web/jsp/amigos.jsp
@@ -1,3 +1,7 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%> 
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%> 
 
 <!doctype html>
 <html lang="en">
@@ -136,7 +140,4 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
-
-
-
 </html>
